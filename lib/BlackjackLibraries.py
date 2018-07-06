b17c97569dc534bfe1edb6bd25f325fefada70dc
Jeffrey Scott <valenpendragon@gmail.com>
2018-07-06 08:05:32 -0400

    Created the Card class and its __init__() method. No testing of the method to see if it works yet.

--- lib/BlackjackLibraries.py
+++ lib/BlackjackLibraries.py
@@ -3,20 +3,70 @@
 Created on Thu Jul  5 07:17:53 2018
 
 This is the principal Class library for Blackjack Games. These classes include:
-    
+
     Class Card: Stores a card, along with its value in Blackjack.
         SubClass Ace: Stores aces, which have two possible values in game.
-        
+
     Class Deck: 52 card object
         SubClass CardShoe: A multideck (1 to 8 decks) object fully shuffled
             with additional entropy added to improve randomness.
-            
+
     Class Hand: A grouping of cards dealt to players.
         SubClass DealerHand: A hand specifically designed for the dealer.
-        
-    Class Player: Stores the hand(sd), bet(s), and bank status of each player
-        SubClass Dealer: Stores the hand of the dealer.
+
+    Class Player: Stores the hand(s), bet(s), and bank status of each player
+        SubClass Dealer: Stores the hand of the dealer and the dealer's bank.'
 
 @author: Jeffrey Scott
 """
 
+
+class Card(object):
+    '''
+    This class is used to simulate a playing card. It is composed of a
+    three attributes: rank, suit, and value, where rank is taken from 2 through
+    king, suit (also found below), and value is based on the rank, being
+    equal to numerical value appearing in the rank. All "face" cards, Jack,
+    Queen, and King, have a value of 10 as well.
+    Note: Aces are dealt with in a subclass.
+
+    Methods:
+        __init__: creates a card tuple using provided rank and suit.
+        __str__: prints out the card in Rank-Suit format.
+    '''
+
+    # Methods
+    def __init__(self, rank, suit):
+        """
+        This method creates a card object from two arguments, rank and suit.
+        If a rank appears that is not in ranks, it will raise a ValueError.
+        INPUTS: rank, suit, both strings
+        OUTPUT: None
+        """
+        # First we need to check the rank. If is not in a specific set of
+        # values, we need to raise an error. We have to explicitly create
+        # a local copy of ranks and suits since they do not exist yet.
+        ranks = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K')
+        suits = ('S', 'D', 'H', 'C')
+        if rank not in ranks:
+            print(f"Card: An invalid rank was supplied {rank}.")
+            raise ValueError("Card objects must have a valid rank.") from None
+            return None
+
+        if suit not in suits:
+            print(f"Card: An invalid suit was supplied {suit}.")
+            raise ValueError("Card objects must have a valid suit.") from None
+            return None
+
+        # If we get to this point, the rank and suit are valid choices.
+        self.rank = rank
+        self.suit = suit
+        # Now, we need to check the value. We will try to convert the rank to
+        # an integer. If successful, we know that the card is 2 to 10. If not,
+        # we know that it is a face card (Aces are dealt with in a subclass).
+        try:
+            value = int(self.rank)
+        except ValueError:
+            # This is a face card.
+            value = 10
+        self.value = value
