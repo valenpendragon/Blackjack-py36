62986d5909f2a60adc886111ec7f55041df09b11
Jeffrey Scott <valenpendragon@gmail.com>
2018-10-18 19:46:59 -0400

    2018-10-18 19:45: Created Class Deck with preamble. Created __init__() method, __len__() method, __str__() method, __del__() method, and remove_top() method. remove_top() removes the card at index 0 and returns it. All of these methods have been fully tested. Cleaned up notes on Classes Card and Ace. Added constants RANKS and SUITS.

--- lib/BlackjackLibraries.py
+++ lib/BlackjackLibraries.py
@@ -18,9 +18,22 @@ This is the principal Class library for Blackjack Games. These classes include:
     Class Player: Stores the hand(s), bet(s), and bank status of each player
         SubClass Dealer: Stores the hand of the dealer and the dealer's bank.'
 
+    Class CasinoTable: Object stores blackjack multipliers, players, the deck
+        and the Dealer. It hands the actual play of rounds of Blackjack.
+
+    Class Casino:  Store CasinoTable objects of various kinds. Methods
+        arbitrate which tables players can play at and controls special events.
+
+    Class Game: This object stores the attributes of the players between games.
+
 @author: Jeffrey Scott
 """
 
+import random as rd
+
+# Constants:
+RANKS = ('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K')
+SUITS = ( 'S', 'D', 'H', 'C')
 
 class Card(object):
     '''
@@ -33,7 +46,15 @@ class Card(object):
 
     Methods:
         __init__: creates a card tuple using provided rank and suit.
-        __str__: prints out the card in Rank-Suit format.
+        __str__: returns the card in Rank-Suit format.
+
+    Attributes:
+        self.rank: This is the rank of the card. Valid values are: '2', '3',
+            '4', '5', '6', '7','8', '9', '10', 'J', 'Q', 'K'.
+            Note: The rank of this base class does not include Aces.
+        self.suit: This is the card suit (Spades, Diamonds, Hearts, Clubs),
+            represented by the first character of the name of the suit.
+        self.value: This is the integer value of the rank (2 - 10).
     '''
 
     # Methods
@@ -74,7 +95,7 @@ class Card(object):
 
     def __str__(self):
         """
-        This method prints out the card in the format Rank-Suit. It suppresses
+        This method returns the card in the format Rank-Suit. It suppresses
         the newline very specifically. It takes no arguments.
         """
         return "{0}-{1} ".format(self.rank, self.suit)
@@ -86,12 +107,28 @@ class Ace(Card):
     possible values in Blackjack, 1 or 11. The value depends on whether or not
     the dealer or player would bust if the Ace is considered an 11. This class
     inherits __str__, but needs a separate __init__() method. In usage in game
-    programming, if statements, like this one?
+    programming, use an if statement like this one:
         if type(card) == Ace:
-    can used to separate Aces from the other cards when scoring hands, etc.
+    to separate Aces from the other cards when scoring hands, etc.
 
     Unique Methods:
         __init__: Adds an extra attribute reflecting an ace's second value.
+            Takes only suit as an argument.
+
+    Inherited Methods:
+        __str__: Returns a value string in Rank-Suit format.
+
+    Unique Attributes:
+        self.additional_value: This is the higher value of a Ace, 11.
+
+    Inherited Attributes:
+        self.rank: This is the rank of the card. Valid values are: '2', '3',
+            '4', '5', '6', '7','8', '9', '10', 'J', 'Q', 'K'.
+            Note: The rank of this base class does not include Aces.
+        self.suit: This is the card suit (Spades, Diamonds, Hearts, Clubs),
+            represented by the first character of the name of the suit.
+        self.value: This is the integer value of the rank (2 - 10).
+
     """
 
     # Methods:
@@ -99,6 +136,7 @@ class Ace(Card):
         """
         Aces only take a str value for suit. They take no other arguments.
         INPUT: suit, string
+        OUTPUT: None
         """
         suits = ('S', 'D', 'H', 'C')
         if suit not in suits:
@@ -110,4 +148,105 @@ class Ace(Card):
         self.rank = 'A'
         self.suit = suit
         self.value = 1
-        self.higher_value = 11
+        self.additional_value = 11
+
+
+class Deck(object):
+    '''
+    This class returns a 52-card shuffled deck consisting of 4 suits, and 13
+    cards per suit, Ace through King.
+    INPUTS: None
+    OUTPUTS: None
+
+    Methods:
+        __init__: returns a shuffled deck of 52 cards. Takes no arguments.
+        __str__: returns the string "A shuffled deck of {length} cards", where
+            length is the length determined by the __len__ function below.
+        __len__: returns the number of cards remaining in the deck.
+        __del__: prints a deck deleted string.
+        deal_card: removes the card at index 0 and shifts the cards up one
+            accordingly. This method takes no arguments.
+    Attributes:
+        shuffled_deck: the contents of the deck (a list of card objects
+        length: The number of cards in the original deck.
+
+    '''
+    def __init__(self):
+        """
+        This method generates a 52-card fully shuffled deck. It uses calls to
+        rd.randint and rd.shuffle to create additional chaos in the shuffling
+        process.
+
+        NOTE: This randomization is good enough for a video game, but it is not
+        random enough for gambling purposes.
+
+        """
+        # This is a single standard deck of 52 cards.
+        self.length = 52
+
+        # Next, we need to create an unshuffled deck to move cards from.
+        deck = []
+        for rank in RANKS:
+            for suit in SUITS:
+                # Create the card.
+                if rank == 'A':
+                    card = Ace(suit)
+                else:
+                    card = Card(rank, suit)
+                deck.append(card)
+
+        # Next, we shuffle it using the rd.shuffle.
+        rd.shuffle(deck)
+
+        # To get some additional entropy, we take this shuffled set of cards
+        # and randomly remove them one at a time and put them in the actual
+        # shuffled deck, self.shuffled_deck.
+        self.shuffled_deck = []
+        while len(deck) > 0:
+            next_card = deck.pop(rd.randint(0, len(deck) - 1))
+            self.shuffled_deck.append(next_card)
+        del deck
+
+    def __len__(self):
+        """
+        This method prints out the number of cards remaining in the Deck.
+        object. It takes no arguments.
+        INPUTS: None
+        OUTPUTS: lenght, integer
+        """
+        return len(self.shuffled_deck)
+
+    def __str__(self, diagnostic=False):
+        """
+        This method prints out the deck. In normal mode, it prints a string
+        with the number of cards reamining in the deck. When diagnostic is
+        True, it will print cards listed in the deck.
+        INPUTS: diagnostic, boolean, defaults to False
+        OUTPUTS: a string indicating remaining cards or an actual printout of
+            the deck to the terminal screen.
+        NOTE: To use the diagnostic option, use the Deck.__str__(**kwargs) form
+            not the print(Deck) or str(Deck) methods.
+        """
+        if not diagnostic:
+            return "The deck has {0} cards remaining.".format(len(self))
+        else:
+            for card in self.shuffled_deck:
+                print(card)
+
+    def __del__(self):
+        """
+        Deletes the deck object and prints a message indicating it has been
+        deleted.
+        INPUTS: None
+        OUTPUTS: None
+        """
+        print("The current deck has been removed from the game.")
+
+    def remove_top(self):
+        """
+        This mechod removes the card at index zero of the Deck object. This is
+        used when dealing cards from the deck.
+        INPUTS: None
+        OUTPUTS: card, Card type object
+        """
+        return self.shuffled_deck.pop(0)
